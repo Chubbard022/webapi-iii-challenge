70247b4d05ed800c6154994e0c616e1237be371a
Chubbard022 <chubbard022@gmail.com>
2019-04-24 12:05:51 -0700

    added mogran,hemet dependencies also added get methods for posts

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const express = require('express'); // importing a CommonJS module
+const express = require('express'); 
+
+const post = require("./data/helpers/postDb");
+const user = require("./data/helpers/userDb");
+
 const server = express();
 
 server.get('/', (req, res, next) => {
@@ -7,5 +11,27 @@ server.get('/', (req, res, next) => {
       <p>World</p>
       `);
   });
+
+//------------GET METHOD POST----------------
+server.get("/posts",(req,res)=>{
+  post.get()
+  .then(posts=>{
+    res.status(201).json(posts)
+  })
+  .catch(err=>{
+    res.status(400).json({error: "cannot GET data"})
+  })
+})
+
+//------------GET METHOD USER----------------
+server.get("/user",(req,res)=>{
+  post.get()
+  .then(user=>{
+    res.status(201).json(user)
+  })
+  .catch(err=>{
+    res.status(400).json({error: "cannot GET data"})
+  })
+})
 
 module.exports = server;
