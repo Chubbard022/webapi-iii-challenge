872b4a275a0b63c20748952867b5ab72ae93b65e
Chubbard022 <chubbard022@gmail.com>
2019-04-24 12:44:45 -0700

    get posts by ID working

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express'); 
+const helmet= require("helmet");
+const morgan = require("morgan");
 
 const post = require("./data/helpers/postDb");
 const user = require("./data/helpers/userDb");
 
 const server = express();
+server.use(helmet());
 
 server.get('/', (req, res, next) => {
     res.send(`
@@ -16,22 +19,37 @@ server.get('/', (req, res, next) => {
 server.get("/posts",(req,res)=>{
   post.get()
   .then(posts=>{
-    res.status(201).json(posts)
+    res.status(200).json(posts)
   })
   .catch(err=>{
     res.status(400).json({error: "cannot GET data"})
+  })
+})
+//------------GET METHOD POST----------------
+server.get("/posts/:id",(req,res)=>{
+    const postId = req.params.id;
+  post.getById(postId)
+  .then(posts=>{
+    res.status(200).json(posts)
+  })
+  .catch(err=>{
+    err.status(400).json({error: "cannot GET id data"})
   })
 })
 
-//------------GET METHOD USER----------------
-server.get("/user",(req,res)=>{
-  post.get()
-  .then(user=>{
-    res.status(201).json(user)
-  })
-  .catch(err=>{
-    res.status(400).json({error: "cannot GET data"})
-  })
-})
 
 module.exports = server;
+
+
+
+
+// //------------GET METHOD USER----------------
+// server.get("/user",(req,res)=>{
+//   user.get()
+//   .then(user=>{
+//     res.status(201).json(user)
+//   })
+//   .catch(err=>{
+//     res.status(400).json({error: "cannot GET data"})
+//   })
+// })
